0bd11cf0da3137664bd10c3c580fd44482ccb9c6
Nicole <nkalexander1@verizon.net>
2020-03-01 10:33:10 -0500

    fix script position

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 // //jquery selections connecting javascript to html
-// var score = 0;
-// $("#start").on("click", () => {
-//     startQuiz({})
-// })
+var score = 0;
+$("#start").on("click", () => {
+    startQuiz({})
+})
+
+
+console.log($("#start"))
 // //for-loop for moving through the game question by question
 
+function startQuiz(){
 
-for(var i=0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-        score++;
-        alert("Correct!");
-    } else {
-        alert("Sorry, that's incorrect")
+    for(var i=0; i < questions.length; i++){
+        var response = window.prompt(questions[i].prompt);
+        if(response == questions[i].answer){
+            score++;
+            alert("Correct!");
+        } else {
+            alert("Sorry, that's incorrect")
+        }
     }
+    alert("You got " + score + "/" + questions.length)
 }
-alert("You got " + score + "/" + questions.length)
